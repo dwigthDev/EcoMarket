832648cf7f73801dc9f1a2a53e83096b4256cfdb
santiago lopez <slopez@grupodmc.cl>
2025-04-23 17:52:25 -0400

    solucion registro

--- src/Routes/Registro.jsx
+++ src/Routes/Registro.jsx
@@ -11,34 +11,26 @@ const [APMAT,setAPMAT] = useState("");
 const [genero,setGenero] = useState("");
 const [nacimiento,setNacimiento] = useState("");
 
-function validarDatos(e){
-    e.preventDefault();
-    function validarDatos(){
-        if (
-            !correo ||
-            !contra ||
-            !nombre ||
-            !APPAT ||
-            !APMAT ||
-            !genero ||
-            !nacimiento
-          ) {
+function validarDatos(){
+            correo  == ""||
+            contra  == ""||
+            nombre == "" ||
+            APPAT  == ""||
+            APMAT  == ""||
+            genero  == ""||
+            nacimiento == ""
             alert("Todos los campos deben ser completados");
-            return false; // Para impedir envío de formulario, si aplica
-          }
-        
+            console.log(`
+                correo:${correo}
+                contraseña:${contra}
+                nombre:${nombre}
+                APPAT:${APPAT}
+                APMAT:${APMAT}
+                genero:${genero}
+                nacimiento:${nacimiento}
+            `)
     }
 
-    console.log(`
-            correo:${correo}
-            contraseña:${contra}
-            nombre:${nombre}
-            APPAT:${APPAT}
-            APMAT:${APMAT}
-            genero:${genero}
-            nacimiento:${nacimiento}
-        `)
-}
 
 
     return(
